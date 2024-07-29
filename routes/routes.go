c5a433d72879dbc2a0d38a80c8eef4e3c0538e53
Lucas Miranda <lucasdef.miranda@gmail.com>
2024-07-29 17:24:59 -0300

    minor cors config added

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Milkado/api-challenge-jornada-milhas/helpers"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func HandleRequest() {
@@ -17,12 +18,15 @@ func HandleRequest() {
 	r.Logger.Fatal(r.Start(helpers.Env("PORT")))
 }
 
+// TODO: if deployed, configure cors to accept only de front domain in .env file
 func routes(echo *echo.Echo) {
 	auth := echo.Group("api")
 	auth.Use(echojwt.JWT([]byte(helpers.Env("JWT_SECRET"))))
+	auth.Use(middleware.CORS())
 	auth.GET("/me", controllers.Me)
 
 	open := echo.Group("api")
+	open.Use(middleware.CORS())
 
 	//Auth
 	open.POST("/login", controllers.Login)
